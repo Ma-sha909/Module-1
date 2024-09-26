eabd8d491a88ad9befc5b162b1515afae488760c
Ma-sha909 <mariart.96.com@gmail.com>
2024-09-26 20:48:52 +0300

    Add files via upload

---
+++ Peremennie.py
@@ -0,0 +1,5 @@
+DZ = 12
+clock = 1.5
+name_kyrs = 'Python'
+DZ_one = (clock / DZ)
+print('Курс:',name_kyrs,',','всего задач: ',DZ,',','затрачено часов: ',clock,',','среднее время выполнения: ',DZ_one,'часа.')
