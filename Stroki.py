f49ce200caed0a6d3ca69eecf0751f3c1506c3eb
Ma-sha909 <mariart.96.com@gmail.com>
2024-09-29 19:56:20 +0300

    Add files via upload

---
+++ Stroki.py
@@ -0,0 +1,12 @@
+example = "Indexing"
+print(example[0])
+print(example[-1])
+print(example[5:])
+print(example[::-1])
+print(example[::2])
+example2 ='Топинамбур'
+print(example2[0])
+print(example2[-1])
+print(example2[5:])
+print(example2[::-1])
+print(example2[1::2])
