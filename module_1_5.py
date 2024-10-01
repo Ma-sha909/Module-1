f4bdcb937fb5fd2b88201feb05ac5567be18c090
Ma-sha909 <mariart.96.com@gmail.com>
2024-10-01 20:39:10 +0300

    Add files via upload

---
+++ module_1_5.py
@@ -0,0 +1,9 @@
+immutable_var = (1, 2, "f")
+print(immutable_var)
+immutable_var [1] = "0"
+print(immutable_var)
+#кортеж неизменяемый вид списка.
+mutable_list = [1, 2, "f", "a",3]
+print(mutable_list)
+mutable_list[0] = "0"
+print(mutable_list)
